662e6164827ae507af2eed5d2710ddc715f940c2
datnguyen2001 <nguyendatdz01@gmail.com>
2024-11-15 10:34:19 +0700

    home

---
+++ resources/views/web/partials/footer.blade.php
@@ -0,0 +1,43 @@
+<div class="box-footer">
+    <div class="footer">
+        <div class="footer-main">
+            <div class="item-footer">
+                <p class="title-footer">Hỗ trợ khách hàng</p>
+                <div class="box-menu-footer">
+                    <a href="#" class="item-menu-footer">Các câu hỏi thường gặp</a>
+                    <a href="#" class="item-menu-footer">Gửi yêu cầu hỗ trợ</a>
+                    <a href="#" class="item-menu-footer">Hướng dẫn đặt hàng</a>
+                    <a href="#" class="item-menu-footer">Hướng dẫn hủy đơn hàng</a>
+                    <a href="#" class="item-menu-footer">Phương thức vận chuyển</a>
+                    <a href="#" class="item-menu-footer">Chính sách đổi trả</a>
+                    <a href="#" class="item-menu-footer">Hỗ trợ khách hàng:lienhe@krmedi.com</a>
+                    <a href="#" class="item-menu-footer">Báo lỗi bảo mật:technical@krmedi.com</a>
+                </div>
+            </div>
+            <div class="item-footer">
+                <p class="title-footer">Về chúng tôi</p>
+                <div class="box-menu-footer">
+                    <a href="#" class="item-menu-footer">Giới thiệu</a>
+                    <a href="#" class="item-menu-footer">Blog Kinh Doanh</a>
+                    <a href="#" class="item-menu-footer">Tuyển dụng</a>
+                    <a href="#" class="item-menu-footer">Điều khoản sử dụng</a>
+                    <a href="#" class="item-menu-footer">Thông tin thanh toán</a>
+                    <a href="#" class="item-menu-footer">Chính sách bảo mật thanh toán</a>
+                    <a href="#" class="item-menu-footer">Chính sách bảo mật thông tin</a>
+                </div>
+            </div>
+            <div class="item-footer">
+                <p class="title-footer">Kết nối với chúng tôi</p>
+                <div class="d-flex align-center justify-content-between">
+                    <a href="#" class="item-menu-footer"><img src="{{asset('assets/images/icon-gg.png')}}" ></a>
+                    <a href="#" class="item-menu-footer"><img src="{{asset('assets/images/icon-fb.png')}}" ></a>
+                    <a href="#" class="item-menu-footer"><img src="{{asset('assets/images/icon-tw.png')}}" ></a>
+                    <a href="#" class="item-menu-footer"><img src="{{asset('assets/images/icon-apple.png')}}" ></a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            © 2023. All Rights Reserved Country & Region: Vietnam | China | Japan | Korea
+        </div>
+    </div>
+</div>
---
+++ resources/views/web/partials/header.blade.php
@@ -0,0 +1,23 @@
+<div class="box-header">
+    <div class="header">
+         <div class="header-main">
+             <div class="title-logo">Gifticon</div>
+             <div class="box-search">
+                 <input type="text" class="input-search" placeholder="Tìm kiếm bất cứ điều gì">
+                 <div class="icon-search"><img src="{{asset('assets/images/icon-search.png')}}" alt=""></div>
+             </div>
+            <div class="d-flex align-items-center">
+                <a href="#" class="link-login">Đăng nhập</a>
+                <a href="#" class="icon-header"><img src="{{asset('assets/images/heart-icon.png')}}" ></a>
+                <a href="#" class="icon-header"><img src="{{asset('assets/images/icon-cart.png')}}" ></a>
+            </div>
+         </div>
+        <div class="header-bottom">
+            <div class="item-menu-header"><img src="{{asset('assets/images/Container.png')}}" alt=""> <span>Gif card</span></div>
+            <a href="#" class="item-menu-header">Tất cả các thương hiệu</a>
+            <a href="#" class="item-menu-header">Quà của tôi</a>
+            <a href="#" class="item-menu-header">Mua số lượng lớn</a>
+            <a href="#" class="item-menu-header">Đăng ký</a>
+        </div>
+    </div>
+</div>
