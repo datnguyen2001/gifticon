394e6d834eecee3f82ff50858c234fc08f3796c1
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2024-11-22 09:32:15 +0700

    update home and shop

--- app/Http/Controllers/web/ShopController.php
+++ app/Http/Controllers/web/ShopController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
+use App\Models\CategoryModel;
+use App\Models\ShopModel;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     public function trademark()
     {
-        return view('web.trademark.index');
+        $categories = CategoryModel::where('display', 1)->get();
+        $brands = ShopModel::where('display', 1)->select('name', 'id', 'src', 'slug')->get();
+        return view('web.trademark.index', compact('categories', 'brands'));
     }
 }
