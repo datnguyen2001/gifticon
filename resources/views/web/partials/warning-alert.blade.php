8181f60bff5f7a2d8a255cba062aefe430471be2
hiepduc24089 <146322141+hiepduc24089@users.noreply.github.com>
2024-12-04 11:14:42 +0700

    update warning

--- resources/views/web/partials/warning-alert.blade.php
+++ resources/views/web/partials/warning-alert.blade.php
@@ -1,5 +1,5 @@
 <style>
-    .failed-overlay {
+    .warning-overlay {
         position: fixed;
         top: 0;
         left: 0;
@@ -8,7 +8,7 @@
         background-color: rgba(0, 0, 0, 0.5);
         z-index: 9998;
     }
-    .failed-alert {
+    .warning-alert {
         position: fixed;
         top: 50%;
         left: 50%;
@@ -21,8 +21,8 @@
         border-radius: 10px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
         z-index: 9999;
-        width: 25%;
-        height: 300px;
+        width: 30%;
+        height: 400px;
     }
 
     .close-popup {
@@ -34,12 +34,12 @@
         color: #333;
     }
 
-    .failed-image {
+    .warning-image {
         width: 200px;
         margin-bottom: 15px;
     }
 
-    .failed-alert p {
+    .warning-alert p {
         font-size: 20px;
         color: #333;
         font-weight: 600;
@@ -47,49 +47,73 @@
         margin: 0;
         padding: 0 20px;
     }
+    .button-to-login{
+        padding: 8px 16px;
+        background: #F1641E;
+        color: white;
+        border-radius: 8px;
+    }
+    .button-back{
+        padding: 6px 16px;
+        background: transparent;
+        border: 1px solid #F1641E;
+        color: #F1641E;
+        border-radius: 8px;
+        font-weight: 600;
+    }
     @media screen and (max-width: 1200px) {
-        .failed-alert {
+        .warning-alert {
             width: 50%;
         }
     }
     @media screen and (max-width: 768px) {
-        .failed-alert {
+        .warning-alert {
             width: 75%;
         }
-        .failed-alert p {
+        .warning-alert p {
             font-size: 16px;
-            padding: 0 10px;
+            padding: 0 10px
+        }
+        .button-to-login{
+            padding: 4px 16px;
+        }
+        .button-back {
+            padding: 2px 16px;
         }
     }
     @media screen and (max-width: 420px) {
-        .failed-alert {
+        .warning-alert {
             width: 100%;
         }
     }
 </style>
 
-<div class="failed-overlay" style="display: none"></div>
-<div class="failed-alert" style="display: none">
+<div class="warning-overlay" style="display: none"></div>
+<div class="warning-alert" style="display: none">
     <div class="close-popup">
         <i class="fa fa-times" aria-hidden="true"></i>
     </div>
-    <img src="{{ asset('assets/images/failed-alert.png') }}" alt="failed" class="failed-image" />
-    <p class="failed-message"></p>
+    <img src="{{ asset('assets/images/warning-alert.png') }}" alt="warning" class="warning-image" />
+    <p class="warning-message"></p>
+    <div class="d-flex align-items-center justify-content-center mt-4" style="gap: 10px">
+        <a href="{{route('login')}}" class="button-to-login">Đăng Nhập</a>
+        <button class="button-back">Quay Lại</button>
+    </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     $(document).ready(function () {
         function hidePopup() {
-            $(".failed-overlay").fadeOut();
-            $(".failed-alert").fadeOut();
+            $(".warning-overlay").fadeOut();
+            $(".warning-alert").fadeOut();
         }
 
-        $(".close-popup").on("click", function () {
+        $(".close-popup, .button-to-login, .button-back").on("click", function () {
             hidePopup();
         });
 
-        $(".failed-overlay").on("click", function () {
+        $(".warning-overlay").on("click", function () {
             hidePopup();
         });
     });
